34959c8c4a41305549e86a8453a816a1ab0b422b
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2017-04-28 04:37:18 +0200

    lex order

--- octostep3.R
+++ octostep3.R
@@ -9,7 +9,6 @@ octostep <- function(x, func, pad=1L) {
   # iterate
   i <- 1L
   repeat {
-    if (i > length(x)) break  # trapdoor
     # build argument list for current callback
     z <- lapply(1L:(2L * pad + 1L), function(a) {
       fixi <- -(pad + 1L - a) + i
@@ -24,6 +23,7 @@ octostep <- function(x, func, pad=1L) {
     rtn <- do.call(func, z)           # do the call
     if (!is.null(rtn)) y[[i]] <- rtn  # dabbing around
     i <- i + 1L                       # increment
+    if (i > length(x)) break          # trapdoor
   }
   # exit
   return(y)
