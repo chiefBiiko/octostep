b98934ac6c65ae2cde8b7a492c185c96801462ca
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2017-05-06 17:40:49 +0200

    choose a side

--- reduceList.R
+++ reduceList.R
@@ -10,6 +10,9 @@
 #' \strong{optional}.
 #' @param allow.ragged Whether to allow input lists of unequal length, 
 #' defaults to \code{FALSE} \strong{optional}.
+#' @param warn Logical indicating whether to signal a warning if the name 
+#' vector specified by \code{which.names} does not have the same length as 
+#' the longest list of \code{x} \strong{optional}.
 #' @return List.
 #' 
 #' @seealso \code{\link{octostep}}
@@ -18,8 +21,9 @@
 reduceList <- function(x, func, 
                        which.names=NULL, 
                        from=c('left', 'right')[1],
-                       allow.ragged=FALSE) {
-  stopifnot(is.list(x), length(x) >= 2L, 
+                       allow.ragged=FALSE, warn=TRUE) {
+  stopifnot(is.list(x), length(x) > 1L, 
+            all(sapply(x, function(xx) length(xx) > 0L)), 
             is.function(func), length(formals(func)) == length(x),
             is.null(which.names) || which.names %in% 1L:length(x), 
             from %in% c('left', 'right'), 
@@ -33,17 +37,23 @@ reduceList <- function(x, func,
   # naming
   if (is.numeric(which.names)) {
     names(y) <- names(x[[which.names]]) 
-    if (anyNA(names(y))) warning('Return value has missing names')
+    if (warn && anyNA(names(y))) warning('Return value has missing names')
   }
   # iterate
-  i <- 1L
+  if (from == 'left') {
+    i <- 1L
+    crem <- +1L
+  } else {
+    i <- maxl
+    crem <- -1L
+  }
   repeat {
     # build argument list for current iteration/callback
-    z <- lapply(x, function(xx) xx[[i]])
-    rtn <- do.call(func, z)           # do the call
-    if (!is.null(rtn)) y[[i]] <- rtn  # dabbing around
-    i <- i + 1L                       # increment
-    if (i > maxl) break               # trapdoor
+    z <- lapply(x, function(xx) if (i %in% 1L:length(xx)) xx[[i]])
+    rtn <- do.call(func, z)                # do the call
+    if (!is.null(rtn)) y[[i]] <- rtn       # dabbing around
+    i <- i + crem                          # crement
+    if (i > maxl || i < 1L) break          # trapdoor
   }
   # exit
   return(y)
